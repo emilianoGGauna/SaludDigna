d509c3778d38e7af23d30f9910d32ae4a6ccf819
emilianoGGauna <120588277+emilianoGGauna@users.noreply.github.com>
2025-05-05 03:11:18 +0000

    cambios

--- DatabaseEDA.py
+++ DatabaseEDA.py
@@ -17,7 +17,8 @@ from typing import Dict, List, Optional
 
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine, inspect, Engine
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.engine import Engine
 from sqlalchemy.exc import OperationalError, SQLAlchemyError
 from azure.core.exceptions import AzureError
 from SecretKeys import SecretKeys
